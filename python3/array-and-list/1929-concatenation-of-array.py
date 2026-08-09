8ce0a47af8690f9b95c466c7cb8d660ec8a138b7
annasehgal <annasehgal05@gmail.com>
2026-08-09 16:49:55 -0700

    dont need len

--- python3/array-and-list/1929-concatenation-of-array.py
+++ python3/array-and-list/1929-concatenation-of-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        n = len(nums)
+        #n = len(nums)
         ans = []
         for num in nums:
             ans.append(num)
